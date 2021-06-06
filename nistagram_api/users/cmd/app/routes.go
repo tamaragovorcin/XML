7acfda4480e33ac7827e9303bc0c94b980f4571b
Lidija Vuković <lidija.vukovic@uns.ac.rs>
2021-06-06 11:47:21 +0200

    Refactor microservices

--- nistagram_api/users/cmd/app/routes.go
+++ nistagram_api/users/cmd/app/routes.go
@@ -44,5 +44,15 @@ func (app *application) routes() *mux.Router {
 	r.HandleFunc("/api/user/profileImage/{userId}", app.saveImage).Methods("POST")
 	r.HandleFunc("/api/user/profileImage/{userId}", app.getUsersProfileImage).Methods("GET")
 
+	r.HandleFunc("/notification/", app.getAllNotification).Methods("GET")
+	r.HandleFunc("/notification/{id}", app.findByIDNotification).Methods("GET")
+	r.HandleFunc("/notification/", app.insertNotification).Methods("POST")
+	r.HandleFunc("/notification/{id}", app.deleteNotification).Methods("DELETE")
+
+	r.HandleFunc("/settings/", app.getAllNotification).Methods("GET")
+	r.HandleFunc("/settings/{id}", app.findByIDNotification).Methods("GET")
+	r.HandleFunc("/settings/", app.insertNotification).Methods("POST")
+	r.HandleFunc("/settings/{id}", app.deleteNotification).Methods("DELETE")
+
 	return r
 }
